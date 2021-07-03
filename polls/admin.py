32437d834498e2fc2a6b1767a4672efb703f0f4f
alzxy <orcexzy@163.com>
2021-07-03 09:48:10 +0800

    andtest learn

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 
 admin.register()
+
+admin.site.register(Question)
+admin.site.register(Choice)
