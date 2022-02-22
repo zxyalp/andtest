a54f5a8cf66bc98fc6f969e038f68175844073b8
tdxz <tdxzwqbgz@outlook.com>
2022-02-23 00:43:24 +0800

    tests.py

--- polls2/tests.py
+++ polls2/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        发布时间未来的时间，was_published_recently() 返回True.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        发布时间时间超过1天，was_published_recently() 返回False.
+        """
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        发布时间时间1天内，was_published_recently() 返回True.
+        """
+        time = timezone.now() + datetime.timedelta(hours=23, minutes=59, seconds=59)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_question(self):
+        """如果没有任何问题，显示错误消息"""
+        response = self.client.get(reverse('polls2:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls2 are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """已发布的过去的问题展示"""
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls2:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_future_question(self):
+        """发布日期为未来的，列表页不展示。"""
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls2:index'))
+        self.assertContains(response, 'No polls2 are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_and_past_question(self):
+        """即使过去和未来发布问题都存在，仅展示过去发布的问题."""
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls2:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_two_past_question(self):
+        """在问题列表页展示多个问题"""
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls2:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1])
+
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """还未发布的问题，在详细视图访问都会返回404未找不到"""
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls2:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """已发布的问题，在详细试图要显示问题内容"""
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls2:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- polls2/views.py
+++ polls2/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -18,56 +19,55 @@ from .models import Question, Choice
 #         'latest_question_list': latest_question_list,
 #     }
 #     return HttpResponse(template.render(context, request))
-
 # # 2.0
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     context = {'latest_question_list': latest_question_list}
 #     return render(request, 'polls2/index.html', context)
-
 # 3.0 通用视图：ListView
 class IndexView(generic.ListView):
     template_name = 'polls2/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 # 1.0
 # """尝试用 get() 函数获取一个对象，如果不存在就抛出 Http404 错误也是一个普遍的流程, 使用快捷函数： get_object_or_404()¶"""
-#
-#
 # def detail(request, question_id):
 #     try:
 #         question = Question.objects.get(pk=question_id)
 #     except Question.DoesNotExist:
 #         raise Http404("Question does not exist")
 #     return render(request, 'polls2/detail.html', {'question': question})
-
 # 2.0
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls2/detail.html', {'question': question})
-
-
 # 3.0 通用视图：DetailView
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls2/detail.html'
     context_object_name = 'wx_question'
 
+    def get_queryset(self):
+        """排除任何尚未发布的问题"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 # 1.0
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls2/results.html', {'question': question})
-
-
 # 2.0
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls2/results.html'
+
+    def get_queryset(self):
+        """排除任何尚未发布的问题"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
