072f1f4b60e15d9c6230f77df4210c6d87667255
tdxz <tdxzwqbgz@outlook.com>
2022-02-24 00:33:39 +0800

    add tests

--- polls2/admin.py
+++ polls2/admin.py
@@ -4,7 +4,12 @@ from .models import Bank, BankCard, Question, Choice
 
 admin.site.register(Bank)
 admin.site.register(BankCard)
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Choice)
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls2/views.py
+++ polls2/views.py
@@ -75,7 +75,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls2/detail.html', {'question': question,
+        return render(request, 'polls2/detail.html', {'wx_question': question,
                                                       'error_message': "You didn't select a choice."})
     else:
         selected_choice.votes += 1
