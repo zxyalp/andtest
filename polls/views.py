99aab2feb41afba4b88c93cb1aff912b39d01b8d
tdxz <tdxzwqbgz@outlook.com>
2022-02-26 00:28:59 +0800

    change polls.

--- polls/views.py
+++ polls/views.py
@@ -76,7 +76,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {'wx_question': question,
-                                                      'error_message': "You didn't select a choice."})
+                                                     'error_message': "You didn't select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
